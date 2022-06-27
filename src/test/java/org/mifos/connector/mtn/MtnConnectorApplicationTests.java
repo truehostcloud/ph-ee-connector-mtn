d815f5210bf9ff11d01af3ecfe9b3136d74bd9e7
Sabin Sheja <62881592+shejasabin@users.noreply.github.com>
2022-06-27 12:30:22 +0200

    initial config

---
+++ src/test/java/org/mifos/connector/mtn/MtnConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mifos.connector.mtn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtnConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
