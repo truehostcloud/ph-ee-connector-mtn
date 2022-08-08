a053e5adf76cf1dcc7d75824a148500fec1af6ff
Sabin Sheja <62881592+shejasabin@users.noreply.github.com>
2022-08-08 11:13:31 +0200

    build and push image

--- src/main/java/org/mifos/connector/mtn/camel/config/CamelProperties.java
+++ src/main/java/org/mifos/connector/mtn/camel/config/CamelProperties.java
@@ -9,7 +9,7 @@ public class CamelProperties {
     public static final String DEPLOYED_PROCESS = "deployedProcess";
 
     public static final String ACCESS_TOKEN = "accessToken";
-    public static final String MPESA_API_RESPONSE = "mpesaApiResponse";
+    public static final String MTN_API_RESPONSE = "mpesaApiResponse";
     public static final String IS_RETRY_EXCEEDED = "isRetryExceeded";
     public static final String IS_TRANSACTION_PENDING = "isTransactionPending";
     public static final String LAST_RESPONSE_BODY = "lastResponseBody";
